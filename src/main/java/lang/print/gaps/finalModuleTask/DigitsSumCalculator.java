c6bb710280d04a72ba951ecf15993d0bc8bd6592
Muslimbek Soliyev <abuabdulloh0709@gmail.com>
2023-01-09 23:05:15 +0500

    final task of module3

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        //5876
+        //5+6+8
+        System.out.println(number/1000+number%10+number/100%10+number/10%10);
     }
 }
